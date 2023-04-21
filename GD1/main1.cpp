4593c3f6989203504dfc36faf4f70ce27029afe2
victorlisman <victor.lisman@protonmail.com>
2023-04-21 18:55:48 +0300

    ex1

--- GD1/main1.cpp
+++ GD1/main1.cpp
@@ -1,48 +1,57 @@
-#include <stack>
 #include <iostream>
+#include <stack>
+#include <array>
 
 using std::cin;
 using std::cout;
 
-bool isNull(int a[], int n)
+struct sums
 {
-    for (int i = 0; i < n; i++)
-        if (a[i] != 0)
-            return false;
-    return true;
-}
-
+    int s1, s2;
+};
 
 int main()
 {
-    int n, cnt = 0;
-    std::stack<int*> s;
-    
+    int n;
+
     cin >> n;
 
-    int a[100][100];
+    std::array<std::array<int, 100>, 100> a;
+    std::stack<sums> s;
 
     for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
             cin >> a[i][j];
 
-        s.push(a[i]);
-    }
+    for (int i = 0; i < n; i++)
+    {
+        sums sum;
+        sum.s1 = 0;
+        sum.s2 = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            sum.s1 += a[j][i];
+            sum.s2 += a[i][j];
+        }
+
+        s.push(sum);
+
+    } 
+
+    int cnt = 1;
 
     while (!s.empty())
     {
-        int *i = s.top();
-        cnt++;
+
+        if (s.top().s1 == n - 1 && s.top().s2 == 0)
+            cout << "mustariu e aici pe poz " << cnt << "\n";
+
+        cout << s.top().s1 << " " << s.top().s2 << "\n";
         s.pop();
 
-        if (isNull(i, n))
-        {
-            cout << "homer here, id: " << cnt - 1;
-            break;
-        }
-        
+        cnt++;
     }
-    
+
 
 }
